3f5a33b1937fd02f4ee430b5b93c4b17272c2d3d
nleva <n.lev.a@ya.ru>
2017-09-22 19:15:33 +0300

    player args updated

--- YoutubeIFrameApi/src/main/java/ru/sendto/gwt/yt/client/Player.java
+++ YoutubeIFrameApi/src/main/java/ru/sendto/gwt/yt/client/Player.java
@@ -17,10 +17,13 @@ public class Player extends JavaScriptObject {
 			return {};
 		}-*/;
 
-		
 		final public native VideoParams setMediaContentUrl(String mediaContentUrl)/*-{
 			this.mediaContentUrl = mediaContentUrl;
 			return this;
+		}-*/;
+
+		final public native String getMediaContentUrl()/*-{
+			return this.mediaContentUrl;
 		}-*/;
 
 		final public native VideoParams setVideoId(String videoId)/*-{
@@ -28,9 +31,17 @@ public class Player extends JavaScriptObject {
 			return this;
 		}-*/;
 
+		final public native String getVideoId()/*-{
+			return this.videoId;
+		}-*/;
+
 		final public native VideoParams setStart(double start)/*-{
 			this.startSeconds = start;
 			return this;
+		}-*/;
+
+		final public native double getStart()/*-{
+			return this.startSeconds;
 		}-*/;
 
 		final public native VideoParams setEnd(double end)/*-{
@@ -38,9 +49,17 @@ public class Player extends JavaScriptObject {
 			return this;
 		}-*/;
 
+		final public native double getEnd()/*-{
+			return this.endSeconds;
+		}-*/;
+
 		final public native VideoParams setQuality(String quality)/*-{
 			this.suggestedQuality = quality;
 			return this;
+		}-*/;
+
+		final public native String getQuality()/*-{
+			return this.suggestedQuality;
 		}-*/;
 
 	}
@@ -52,9 +71,14 @@ public class Player extends JavaScriptObject {
 		static public native VideoListParams get()/*-{
 			return {};
 		}-*/;
+
 		final public native VideoListParams setListType(String listType)/*-{
 			this.listType = listType;
 			return this;
+		}-*/;
+
+		final public native String getListType()/*-{
+			return this.listType;
 		}-*/;
 
 		final public native VideoListParams setList(String list)/*-{
@@ -62,9 +86,17 @@ public class Player extends JavaScriptObject {
 			return this;
 		}-*/;
 
+		final public native String getList()/*-{
+			return this.list;
+		}-*/;
+
 		final public native VideoListParams setIndex(Number index)/*-{
 			this.index = index;
 			return this;
+		}-*/;
+
+		final public native Number getIndex()/*-{
+			return this.index;
 		}-*/;
 
 		final public native VideoListParams setStartSeconds(Number startSeconds)/*-{
@@ -72,9 +104,17 @@ public class Player extends JavaScriptObject {
 			return this;
 		}-*/;
 
+		final public native Number getStartSeconds()/*-{
+			return this.startSeconds;
+		}-*/;
+
 		final public native VideoListParams setSuggestedQuality(String suggestedQuality)/*-{
 			this.suggestedQuality = suggestedQuality;
 			return this;
+		}-*/;
+
+		final public native String getSuggestedQuality()/*-{
+			return this.suggestedQuality;
 		}-*/;
 	}
 
@@ -86,19 +126,16 @@ public class Player extends JavaScriptObject {
 	// return this.addCueRange();
 	// }-*/;
 
-	
 	final public native Player cuePlaylist(VideoListParams params)/*-{
 		this.cuePlaylist(params);
 		return this;
 	}-*/;
 
-	
 	final public native Player cueVideoById(VideoParams params)/*-{
 		this.cueVideoById(params);
 		return this;
 	}-*/;
 
-	
 	final public native Player cueVideoByUrl(VideoParams params)/*-{
 		this.cueVideoByUrl(params);
 		return this;
@@ -109,27 +146,22 @@ public class Player extends JavaScriptObject {
 	// return this.getApiInterface();
 	// }-*/;
 
-	
 	final public native String[] getAvailablePlaybackRates()/*-{
 		return this.getAvailablePlaybackRates();
 	}-*/;
 
-	
 	final public native String[] getAvailableQualityLevels()/*-{
 		return this.getAvailableQualityLevels();
 	}-*/;
 
-	
 	final public native double getCurrentTime()/*-{
 		return this.getCurrentTime();
 	}-*/;
 
-	
 	final public native String getDebugText()/*-{
 		return this.getDebugText();
 	}-*/;
 
-	
 	final public native double getDuration()/*-{
 		return this.getDuration();
 	}-*/;
@@ -139,32 +171,26 @@ public class Player extends JavaScriptObject {
 		return this.getMediaReferenceTime();
 	}-*/;
 
-	
 	final public native String getPlaybackQuality()/*-{
 		return this.getPlaybackQuality();
 	}-*/;
 
-	
 	final public native String getPlaybackRate()/*-{
 		return this.getPlaybackRate();
 	}-*/;
 
-	
 	final public native int getPlayerState()/*-{
 		return this.getPlayerState();
 	}-*/;
 
-	
 	final public native String[] getPlaylist()/*-{
 		return this.getPlaylist();
 	}-*/;
 
-	
 	final public native String getPlaylistId()/*-{
 		return this.getPlaylistId();
 	}-*/;
 
-	
 	final public native int getPlaylistIndex()/*-{
 		return this.getPlaylistIndex();
 	}-*/;
@@ -174,32 +200,27 @@ public class Player extends JavaScriptObject {
 		return this.getVideoData();
 	}-*/;
 
-	
 	final public native double getVideoLoadedFraction()/*-{
 		return this.getVideoLoadedFraction();
 	}-*/;
-	
+
 	final public native String getPlayerId()/*-{
 		return this.getIframe().id;
 	}-*/;
 
-	
 	final public native String getVideoUrl()/*-{
 		return this.getVideoUrl();
 	}-*/;
 
-	
 	final public native int getVolume()/*-{
 		return this.getVolume();
 	}-*/;
 
-	
 	final public native Player hideVideoInfo()/*-{
 		this.hideVideoInfo();
 		return this;
 	}-*/;
 
-	
 	final public native boolean isMuted()/*-{
 		return this.isMuted();
 	}-*/;
@@ -210,124 +231,105 @@ public class Player extends JavaScriptObject {
 		return this;
 	}-*/;
 
-	
 	final public native Player loadPlaylist(VideoListParams params)/*-{
 		loadPlaylist(params);
 		return this;
 	}-*/;
 
-	
 	final public native void loadVideoById(VideoParams params)/*-{
 		this.loadVideoById(params);
 	}-*/;
 
-	
 	final public native Player loadVideoByUrl(VideoParams params)/*-{
 		this.loadVideoByUrl(params);
 		return this;
 	}-*/;
 
-	
 	final public native Player mute()/*-{
 		this.mute();
 		return this;
 	}-*/;
 
-	
 	final public native Player nextVideo()/*-{
 		this.nextVideo();
 		return this;
 	}-*/;
 
-	
 	final public native Player pauseVideo()/*-{
 		this.pauseVideo();
 		return this;
 	}-*/;
 
-	
 	final public native Player playVideo()/*-{
 		this.playVideo();
 		return this;
 	}-*/;
 
-	
 	final public native Player playVideoAt(int videoInList)/*-{
 		this.playVideoAt(videoInList);
 		return this;
 	}-*/;
 
-	
 	final public native Player previousVideo()/*-{
 		this.previousVideo();
 		return this;
 	}-*/;
 
-	//TODO no doc
-//	final public native void removeCueRange()/*-{
-//		return this.removeCueRange();
-//	}-*/;
+	// TODO no doc
+	// final public native void removeCueRange()/*-{
+	// return this.removeCueRange();
+	// }-*/;
 
 	// TODO is needed?
-//	final public native void removeEventListener()/*-{
-//		return this.removeEventListener();
-//	}-*/;
+	// final public native void removeEventListener()/*-{
+	// return this.removeEventListener();
+	// }-*/;
 
-	
 	final public native Player seekTo(double seconds, boolean offline)/*-{
 		this.seekTo(seconds, !offline);
 		return this;
 	}-*/;
 
-	
 	final public native Player setLoop(boolean loop)/*-{
 		this.setLoop(loop);
 		return this;
 	}-*/;
 
-	
 	final public native Player setOption(String module, String option, String value)/*-{
 		this.setOption(module, option, value);
 		return this;
 	}-*/;
 
-	
 	final public native Player setPlaybackQuality(String quality)/*-{
 		this.setPlaybackQuality(quality);
 		return this;
 	}-*/;
 
-	
 	final public native Player setPlaybackRate(float rate)/*-{
 		this.setPlaybackRate(rate);
 		return this;
 	}-*/;
 
-	
 	final public native Player setShuffle(boolean shuffle)/*-{
 		this.setShuffle(shuffle);
 		return this;
 	}-*/;
 
-	
 	final public native Player setVolume(int vol)/*-{
 		this.setVolume(vol);
 		return this;
 	}-*/;
 
-	
 	final public native Player showVideoInfo()/*-{
 		this.showVideoInfo();
 		return this;
 	}-*/;
 
-	
 	final public native Player stopVideo()/*-{
 		this.stopVideo();
 		return this;
 	}-*/;
 
-	
 	final public native Player unMute()/*-{
 		this.unMute();
 		return this;
